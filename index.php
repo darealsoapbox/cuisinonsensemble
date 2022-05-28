a92f282ae292692c001058ee1d0d4b657411a0fb
darealsoapbox <nicolas@wpandco.fr>
2022-05-28 14:38:28 +0200

    include_once commentés

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php incluce_once('header.php'); ?>
+<?php //incluce_once('header.php'); ?>
 
 <main>
-    
+
 </main>
 
-<?php incluce_once('footer.php'); ?>
+<?php //incluce_once('footer.php'); ?>
