5a862c1832f7418980b2ead71434c0add13b13c6
darealsoapbox <nicolas@wpandco.fr>
2022-05-28 14:44:14 +0200

    page d'accueil + page recette

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
-<?php //incluce_once('header.php'); ?>
+<?php incluce_once('header.php'); ?>
 
 <main>
+    <section>
+        <div class="bloc_1_colonne_gauche">
+            <h2 class="titre">Recettes</h2>
+            <p>
+                <a href="recettes.php">Toutes les recettes</a>
+            </p>
+            <p>
+                <a href="recettes.php?categorie=entree">Entrées</a>
+            </p>
+            <p>
+                <a href="recettes.php?categorie=plat">Plats</a>
+            </p>
+            <p>
+                <a href="recettes.php?categorie=dessert">Desserts</a>
+            </p>
+        </div>
 
+        <div class="bloc_1_colonne_droite">
+            <h2 class="titre">Ajoutez votre recette</h2>
+        </div>
+    </section>
 </main>
 
-<?php //incluce_once('footer.php'); ?>
+<?php incluce_once('footer.php'); ?>
---
+++ recettes.php
@@ -0,0 +1,5 @@
+<?php incluce_once('header.php'); ?>
+<main>
+    
+</main>
+<?php incluce_once('footer.php'); ?>
